5a75f9390d92af8c11ee39b30f7e944d74755a2c
Andres <andrescalvo01@gmail.com>
2025-12-01 13:46:37 +0100

    feat: Creating RepairTask class in where are the values task, cost, etc...

---
+++ workshop-management-system/src/main/java/com/workshop/model/RepairTask.java
@@ -0,0 +1,24 @@
+package com.workshop.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RepairTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String task;
+    private Double cost;
+
+    @ManyToOne
+    @JoinColumn(name = "repair_order_id")
+    private RepairOrder repairOrder;
+}
+
