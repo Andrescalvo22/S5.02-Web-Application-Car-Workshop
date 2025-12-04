f80b7b076e39caf651f3fa4b3b4968b05c131cf8
Andres <andrescalvo01@gmail.com>
2025-12-04 10:31:27 +0100

    feat: Creating User class to create the Entity that will be used in the api as user

---
+++ workshop-management-system/src/main/java/com/workshop/model/User.java
@@ -0,0 +1,35 @@
+package com.workshop.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email; // used as username
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    private Boolean enabled = true;
+
+    private Long customerId;
+}
+
