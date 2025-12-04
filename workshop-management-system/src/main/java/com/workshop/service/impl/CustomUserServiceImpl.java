23de521e1fc2bd6cd4723b9e552993e77ee77fa4
Andres <andrescalvo01@gmail.com>
2025-12-04 11:24:40 +0100

    feat: Creating CustomUserServiceImpl in where implements the interface UserDetailsService and creates the method loadUserByUsername

---
+++ workshop-management-system/src/main/java/com/workshop/service/impl/CustomUserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.workshop.service.impl;
+
+import com.workshop.model.User;
+import com.workshop.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public CustomUserServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = repository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+
+        var authorities = user.getRoles().stream()
+                .map(r -> new SimpleGrantedAuthority(r.name()))
+                .collect(Collectors.toList());
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities(authorities)
+                .accountLocked(false)
+                .disabled(false)
+                .build();
+    }
+}
+
