8f3b915f53b4e706d8a21f3bfec9c9c6d5914bb1
Andres <andrescalvo01@gmail.com>
2025-12-09 11:31:41 +0100

    feat: Adding AllArgsConstructor inr RepairTaskDTO to use it in Mockito testing

--- workshop-management-system/src/main/java/com/workshop/dto/RepairTaskDTO.java
+++ workshop-management-system/src/main/java/com/workshop/dto/RepairTaskDTO.java
@@ -1,10 +1,14 @@
 package com.workshop.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RepairTaskDTO {
     private Long id;
     private String task;
