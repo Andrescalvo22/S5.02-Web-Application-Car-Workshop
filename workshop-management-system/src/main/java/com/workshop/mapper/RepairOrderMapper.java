15e4dbb13d1c728f3210e44fab591ae16ab0491a
Andres <andrescalvo01@gmail.com>
2026-01-09 03:52:02 +0100

    feat: Deleting not used carId value

--- workshop-management-system/src/main/java/com/workshop/mapper/RepairOrderMapper.java
+++ workshop-management-system/src/main/java/com/workshop/mapper/RepairOrderMapper.java
@@ -1,7 +1,6 @@
 package com.workshop.mapper;
 
 import com.workshop.dto.RepairOrderDTO;
-import com.workshop.model.Car;
 import com.workshop.model.RepairOrder;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,17 +8,9 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface RepairOrderMapper {
 
-    @Mapping(source = "car.id", target = "carId")
+    @Mapping(source = "car", target = "car")
     RepairOrderDTO toDTO(RepairOrder order);
 
-    @Mapping(target = "car", expression = "java(toCar(dto.getCarId()))")
+    @Mapping(target = "car", ignore = true)
     RepairOrder toEntity(RepairOrderDTO dto);
-
-    default Car toCar(Long id) {
-        if (id == null) return null;
-        Car car = new Car();
-        car.setId(id);
-        return car;
-    }
 }
-
